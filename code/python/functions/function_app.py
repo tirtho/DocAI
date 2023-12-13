aaf91e8d8a8da2415ae6e6b9ea306dd4ca94f421
Tirthankar Barari <tirtho@hotmail.com>
2023-12-13 18:22:46 -0500

    moved fr API key to app setting and tested

--- code/python/functions/function_app.py
+++ code/python/functions/function_app.py
@@ -1,7 +1,12 @@
+import os
 import azure.functions as func
 import logging
 import uuid
 import json
+from azure.identity import DefaultAzureCredential
+from azure.core.credentials import AzureKeyCredential
+from azure.ai.formrecognizer import DocumentAnalysisClient
+from azure.storage.blob import BlobServiceClient, BlobClient, ContainerClient
 
 app = func.FunctionApp(http_auth_level=func.AuthLevel.ANONYMOUS)
 
@@ -21,7 +26,7 @@ def saveEmailMetadataInCosmosDB(
         logging.info(f"Processing message id {messageId}")
     else:
         errorMessage = "Please send the messageId in the HTTP request"
-        logging.error(errorMessage)
+        logging.info(errorMessage)
         return func.HttpResponse(errorMessage, status_code=400)
 
     req_body = req.get_json()
@@ -30,51 +35,84 @@ def saveEmailMetadataInCosmosDB(
     else:
         # No valid email message content
         errorMessage = "Please send HTTP request body"
-        logging.error(errorMessage)
+        logging.info(errorMessage)
         return func.HttpResponse(errorMessage, status_code=400)
-
+    classes = ["none"]
     try:
         messageUri = req_body.get('uri')
         messageType = req_body.get('messageType')        
-        if messageUri:       
-            logging.info(f'messageUri:{messageUri}')
-        else:
-            # No valid email message content
-            errorMessage = f"Please send the messageUri in the HTTP request body."
-            logging.error(errorMessage)
-            return func.HttpResponse(errorMessage, status_code=400)
-        if messageType:
-            logging.info(f'messageType: {messageType}')
-        else:
-            # No valid email message content
-            errorMessage = f"Please send the messageType in the HTTP request body."
-            logging.error(errorMessage)
-            return func.HttpResponse(errorMessage, status_code=400)
-        receivedTime = req_body.get('receivedTime')
+        receivedTimeFolder = req_body.get('receivedTimeFolder')
         sender = req_body.get('from')
         if messageType == 'email-body':
-            url = messageUri + sender + "/" + receivedTime + "/" + "EmailBody.msg"
+            url = messageUri + sender + "/" + receivedTimeFolder + "/" + "EmailBody.msg"
+            # TODO Classify email message
+            try:
+                logging.info("Classify email message")
+                classes.clear()
+                classes.append(f"class[notYetImplemented,0.0];pages[0,0]")
+            except Exception as e:
+                logging.info(f'Exception raised trying to classify email: {e}')
+                # TODO: return with error
         else:
-            url = messageUri + sender + "/" + receivedTime + "/attachments/" + req_body.get('attachmentName')
-    except ValueError:
-        errorMessage = "Raised ValueError when trying to read HttpRequest message body"
-        logging.error(errorMessage)
+            theAttachmentName = req_body.get('attachmentName')
+            if theAttachmentName:
+                logging.info(f"Attachment name {theAttachmentName}")                
+                url = messageUri + sender + "/" + receivedTimeFolder + "/attachments/" + theAttachmentName
+                try:
+                    frEndpoint = os.environ['FORM_RECOGNIZER_ENDPOINT']
+                    documentClassifierId = os.environ['DOCUMENT_CLASSIFIER_ID']
+                    formRecognizerAPIKey = os.environ['FORM_RECOGNIZER_API_KEY']
+                    logging.info(f"Document class from [{frEndpoint}] classifier id [{documentClassifierId}]")
+                    if formRecognizerAPIKey:
+                        # Using API key
+                        formRecognizerCredential = AzureKeyCredential(formRecognizerAPIKey)
+                    else:
+                        # Using Managed Identity
+                        formRecognizerCredential = DefaultAzureCredential()                    
+                    logging.info("Got Form Recognizer access token")
+                    formRecognizerClient = DocumentAnalysisClient(endpoint=frEndpoint,
+                                                                credential=formRecognizerCredential)
+                    poller = formRecognizerClient.begin_classify_document_from_url(
+                                classifier_id=documentClassifierId,
+                                document_url=url
+                            )
+                    
+                    classifications = poller.result().documents
+                    logging.info(f"Got classifications for attachment from Azure FR: {classifications}")
+                    classes.clear()
+                    for doc in classifications:
+                        identifiedClass = f"class[{doc.doc_type or 'N/A'},{doc.confidence}];pages[{[region.page_number for region in doc.bounding_regions]}]"
+                        classes.append(identifiedClass)
+                except Exception as e:
+                    logging.info(f"Exception raised trying to get classification for attachment {theAttachmentName}: {e}")
+                # TODO :
+                # - Extract form fields, or key/value using Form Recognizer
+                # - Extract data using AOAI
+            else:
+                logging.info("Attachment name not passed in request body by logic app/client")
+    except Exception as e:
+        errorMessage = f"When trying to read HttpRequest request body raised exception: {e}"
+        logging.info(errorMessage)
         return func.HttpResponse(errorMessage, status_code=400)
-
+    
     doc = {
         "id":str(uuid.uuid4()),
         "messageId":messageId,
         "url":url,
+        "classes":classes,
         "messageDetails":req_body
     }
     logging.info('Before json dump')
     try:
         inJSON = json.dumps(doc)
     except:
-        logging.info("Failed to convert request body to json")
-    logging.info(f'Document to store in CosmosDB: {inJSON}')
+        errorMessage = f"Failed to convert request body to json raising exception: {e}"
+        logging.info(errorMessage)
+        return func.HttpResponse(errorMessage, status_code=400)
+
+    logging.info(f"Document to store in CosmosDB: {inJSON}")
     outputDocument.set(func.Document.from_json(inJSON))
     msg.set(messageId)
-    responseMessage = f'{messageId} data stored in Cosmos DB'
+    responseMessage = f"{messageId} data stored in Cosmos DB"
     logging.info(responseMessage)
     return func.HttpResponse(responseMessage, status_code=201)

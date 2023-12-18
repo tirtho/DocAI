f39f271670cfb0042a814da94539391d0e6eed2c
Tirthankar Barari <tirtho@hotmail.com>
2023-12-18 17:28:52 -0500

    getEmailClass, getAttachmentClass, saveEmailProperties, saveAttachmentProperties Functions are working in Az Function python v2 now

--- code/python/functions/function_app.py
+++ code/python/functions/function_app.py
@@ -1,8 +1,9 @@
-import os
 import azure.functions as func
 import logging
 import uuid
 import json
+import os
+
 from azure.identity import DefaultAzureCredential
 from azure.core.credentials import AzureKeyCredential
 from azure.ai.formrecognizer import DocumentAnalysisClient
@@ -10,109 +11,267 @@ from azure.storage.blob import BlobServiceClient, BlobClient, ContainerClient
 
 app = func.FunctionApp(http_auth_level=func.AuthLevel.ANONYMOUS)
 
-@app.route(route="saveEmailMetadataInCosmosDB")
-@app.queue_output(arg_name="msg", queue_name="outqueue", connection="AzureWebJobsStorage")
-@app.cosmos_db_output(arg_name="outputDocument", database_name="DocAIDatabase", 
-container_name="EmailExtracts", connection="CosmosDbConnectionString")
-def saveEmailMetadataInCosmosDB(
-    req: func.HttpRequest,
-    msg: func.Out[func.QueueMessage],
-    outputDocument: func.Out[func.Document]) -> func.HttpResponse:
-
-    logging.info('Python HTTP trigger function processed a request.')
-
-    messageId = req.params.get('messageId')
+def getHttpRequestBody(request, functionName):
+    logging.info(f'{functionName}Received HTTP request from Azure HTTP triggered Function')
+    try:
+        messageId = request.params.get('messageId')
+    except Exception as e:
+        errorMessage = f"{functionName}Error: Trying to get messageId got Exception:{e}"
+        logging.error(errorMessage)
+        raise   
     if messageId:
-        logging.info(f"Processing message id {messageId}")
+        logging.info(f"{functionName}messageId:{messageId}")
     else:
-        errorMessage = "Please send the messageId in the HTTP request"
-        logging.info(errorMessage)
-        return func.HttpResponse(errorMessage, status_code=400)
-
-    req_body = req.get_json()
-    if req_body:
-        logging.info(f"Request body: {req_body}")
+        errorMessage = f"{functionName}Error: messageId not found in HTTP request"
+        logging.error(errorMessage)
+        raise ValueError(errorMessage)   
+    try:
+        theRequestBodyJson = request.get_json()
+    except Exception as e:
+        errorMessage = f"{functionName}Error: Trying to get request body got Exception:{e}"
+        logging.error(errorMessage)
+        raise
+    if theRequestBodyJson:
+        logging.info(f'{functionName}Request body:{theRequestBodyJson}')
     else:
         # No valid email message content
-        errorMessage = "Please send HTTP request body"
-        logging.info(errorMessage)
-        return func.HttpResponse(errorMessage, status_code=400)
-    classes = ["none"]
+        errorMessage = f'{functionName}Error: Request body not found in HTTP request'
+        logging.error(errorMessage)
+        raise ValueError(errorMessage)
+    return messageId, theRequestBodyJson
+
+def getItemFromRequestBody(requestBody, itemName, functionName):
     try:
-        messageUri = req_body.get('uri')
-        messageType = req_body.get('messageType')        
-        receivedTimeFolder = req_body.get('receivedTimeFolder')
-        sender = req_body.get('from')
-        if messageType == 'email-body':
-            url = messageUri + sender + "/" + receivedTimeFolder + "/" + "EmailBody.msg"
-            # TODO Classify email message
-            try:
-                logging.info("Classify email message")
-                classes.clear()
-                classes.append(f"class[notYetImplemented,0.0];pages[0,0]")
-            except Exception as e:
-                logging.info(f'Exception raised trying to classify email: {e}')
-                # TODO: return with error
-        else:
-            theAttachmentName = req_body.get('attachmentName')
-            if theAttachmentName:
-                logging.info(f"Attachment name {theAttachmentName}")                
-                url = messageUri + sender + "/" + receivedTimeFolder + "/attachments/" + theAttachmentName
-                try:
-                    frEndpoint = os.environ['FORM_RECOGNIZER_ENDPOINT']
-                    documentClassifierId = os.environ['DOCUMENT_CLASSIFIER_ID']
-                    formRecognizerAPIKey = os.environ['FORM_RECOGNIZER_API_KEY']
-                    logging.info(f"Document class from [{frEndpoint}] classifier id [{documentClassifierId}]")
-                    if formRecognizerAPIKey:
-                        # Using API key
-                        formRecognizerCredential = AzureKeyCredential(formRecognizerAPIKey)
-                    else:
-                        # Using Managed Identity
-                        formRecognizerCredential = DefaultAzureCredential()                    
-                    logging.info("Got Form Recognizer access token")
-                    formRecognizerClient = DocumentAnalysisClient(endpoint=frEndpoint,
-                                                                credential=formRecognizerCredential)
-                    poller = formRecognizerClient.begin_classify_document_from_url(
-                                classifier_id=documentClassifierId,
-                                document_url=url
-                            )
-                    
-                    classifications = poller.result().documents
-                    logging.info(f"Got classifications for attachment from Azure FR: {classifications}")
-                    classes.clear()
-                    for doc in classifications:
-                        identifiedClass = f"class[{doc.doc_type or 'N/A'},{doc.confidence}];pages[{[region.page_number for region in doc.bounding_regions]}]"
-                        classes.append(identifiedClass)
-                except Exception as e:
-                    logging.info(f"Exception raised trying to get classification for attachment {theAttachmentName}: {e}")
-                # TODO :
-                # - Extract form fields, or key/value using Form Recognizer
-                # - Extract data using AOAI
-            else:
-                logging.info("Attachment name not passed in request body by logic app/client")
+        item = ""
+        item = requestBody.get(itemName)
+        logging.info(f'{functionName}From http request body found {itemName}={item}')
+        return item
     except Exception as e:
-        errorMessage = f"When trying to read HttpRequest request body raised exception: {e}"
-        logging.info(errorMessage)
+        errorMessage = f'{functionName}Error: {itemName} not found in request body'
+        logging.error(errorMessage)
+        raise
+
+def getEmailClassesFromOpenAI(subject, body, fName):
+    # TODO get class from OpenAI
+    logging.info(f'{fName}Calling OpenAI to get email classes')
+    return f'class[new-auto-insurance-claim,0.99]'
+def getAttachmentClassesFromFormRecognizer(attachmentUrl, fName):
+    logging.info(f'{fName}Calling Document Intelligence to get attachment classes')
+    classes = ["unknown"]
+    try:
+        frEndpoint = os.environ['FORM_RECOGNIZER_ENDPOINT']
+        documentClassifierId = os.environ['DOCUMENT_CLASSIFIER_ID']
+        formRecognizerAPIKey = os.environ['FORM_RECOGNIZER_API_KEY']
+        logging.info(f'{fName}REading document class from [{frEndpoint}] classifier id [{documentClassifierId}]')
+        if formRecognizerAPIKey:
+            # Using API key
+            formRecognizerCredential = AzureKeyCredential(formRecognizerAPIKey)
+        else:
+            # Using Managed Identity
+            formRecognizerCredential = DefaultAzureCredential()                    
+            logging.info(f'{fName}Got Form Recognizer access token')
+        formRecognizerClient = DocumentAnalysisClient(
+                                    endpoint=frEndpoint,
+                                    credential=formRecognizerCredential
+                                )
+        poller = formRecognizerClient.begin_classify_document_from_url(
+                            classifier_id=documentClassifierId,
+                            document_url=attachmentUrl
+                )  
+        classifications = poller.result().documents
+        logging.info(f'{fName}For attachment at {attachmentUrl} Document Intelligence returned classes:{classifications}')
+        classes.clear()
+        for doc in classifications:
+            identifiedClass = f"class[{doc.doc_type or 'N/A'},{doc.confidence}];pages[{[region.page_number for region in doc.bounding_regions]}]"
+            classes.append(identifiedClass)
+    except Exception as e:
+        logging.error(f'{fName}ERROR:Get classes for {attachmentUrl} raised exception: {e}')
+        raise
+    return f'{classes}'
+
+@app.route(route="getEmailClass", auth_level=func.AuthLevel.ANONYMOUS)
+def getEmailClass(req: func.HttpRequest) -> func.HttpResponse:
+    fName = f"f(getEmailClass)->"
+    try:
+        messageId, reqBody = getHttpRequestBody(request = req, functionName=fName)
+        logging.info(f'{fName}Received MessageId:{messageId}')
+        logging.info(f'{fName}Received request body:{reqBody}')
+    except Exception as httpRequestErrorMessage:
+        return func.HttpResponse(f'{httpRequestErrorMessage}', status_code=400)
+    try:
+        messageType = getItemFromRequestBody(reqBody, 'messageType', fName)
+        emailPlainBody = getItemFromRequestBody(reqBody, 'body', fName)
+        emailSubject = getItemFromRequestBody(reqBody, 'subject', fName)
+        if messageType == 'email-body':
+            emailClasses = getEmailClassesFromOpenAI(emailSubject, emailPlainBody, fName)
+        else:
+            errorMessage = f'{fName}ERROR: incorrect messageType {messageType}'
+            logging.error(errorMessage)
+            return func.HttpResponse(errorMessage, status_code=400)
+    except Exception as httpRequestErrorMessage:
+        errorMessage = f'{fName}ERROR: Hit exception trying to read request body items. Exception:{httpRequestErrorMessage}'
+        logging.error(errorMessage)
         return func.HttpResponse(errorMessage, status_code=400)
-    
+
+    logging.info(f'{fName}Success: email classes: {emailClasses}') 
+    return func.HttpResponse(f'{emailClasses}', status_code=200)    
+
+@app.route(route="saveEmailProperties", auth_level=func.AuthLevel.ANONYMOUS)
+@app.queue_output(arg_name="msg", queue_name="outqueue", connection="AzureWebJobsStorage")
+@app.cosmos_db_output(arg_name="outputDocument", database_name="DocAIDatabase", 
+    container_name="EmailExtracts", connection="CosmosDbConnectionString")
+def saveEmailProperties(req: func.HttpRequest,
+                        msg: func.Out[func.QueueMessage], 
+                        outputDocument: func.Out[func.Document]) -> func.HttpResponse:
+    fName = f"f(saveEmailProperties)->"
+    try:
+        messageId, reqBody = getHttpRequestBody(request = req, functionName=fName)
+        logging.info(f'{fName}Received MessageId:{messageId}')
+        logging.info(f'{fName}Received request body:{reqBody}')
+    except Exception as httpRequestErrorMessage:
+        return func.HttpResponse(f'{httpRequestErrorMessage}', status_code=400)
+    try:
+        messageType = getItemFromRequestBody(reqBody, 'messageType', fName)        
+        if messageType == 'email-body':
+            receivedTimeFolder = getItemFromRequestBody(reqBody, 'receivedTimeFolder', fName)
+            receivedTime = getItemFromRequestBody(reqBody, 'receivedTime', fName)
+            sender = getItemFromRequestBody(reqBody, 'from', fName)
+            emailClasses = getItemFromRequestBody(reqBody, 'classes', fName)
+            messageUri = getItemFromRequestBody(reqBody, 'uri', fName)
+            url = messageUri + sender + "/" + receivedTimeFolder + "/" + "EmailBody.msg"
+            isHTML = getItemFromRequestBody(reqBody, 'isHTML', fName)
+            bodyPreview = getItemFromRequestBody(reqBody, 'bodyPreview', fName)
+            subject = getItemFromRequestBody(reqBody, 'subject', fName)
+            hasAttachment = getItemFromRequestBody(reqBody, 'hasAttachment', fName)
+        else:
+            errorMessage = f'{fName}ERROR: incorrect messageType {messageType}'
+            logging.error(errorMessage)
+            return func.HttpResponse(errorMessage, status_code=400)
+    except Exception as httpRequestErrorMessage:
+        errorMessage = f'{fName}ERROR: Read request body items raised exception:{httpRequestErrorMessage}'
+        logging.error(errorMessage)
+        return func.HttpResponse(errorMessage, status_code=400)   
     doc = {
         "id":str(uuid.uuid4()),
         "messageId":messageId,
+        "messageType":messageType,
+        "receivedTime":receivedTime,
+        "receivedTimeFolder":receivedTimeFolder,
+        "from":sender,
+        "classes":emailClasses,
         "url":url,
-        "classes":classes,
-        "messageDetails":req_body
+        "isHTML":isHTML,
+        "bodyPreview":bodyPreview,
+        "subject":subject,
+        "hasAttachment":hasAttachment,
+        "messageDetails":reqBody
     }
-    logging.info('Before json dump')
+    logging.info(f'{fName}Creating response payload json')
     try:
         inJSON = json.dumps(doc)
-    except:
-        errorMessage = f"Failed to convert request body to json raising exception: {e}"
-        logging.info(errorMessage)
+        logging.info(f'{fName}Document to store in CosmosDB: {inJSON}')
+    except Exception as je:
+        errorMessage = f'{fName}Error: json dump pf response payload failed. Exception:{je}'
+        logging.error(errorMessage)
         return func.HttpResponse(errorMessage, status_code=400)
-
-    logging.info(f"Document to store in CosmosDB: {inJSON}")
+    
     outputDocument.set(func.Document.from_json(inJSON))
     msg.set(messageId)
-    responseMessage = f"{messageId} data stored in Cosmos DB"
+    responseMessage = f'{fName}messageId  {messageId} data stored in Cosmos DB'
+    logging.info(responseMessage)
+    return func.HttpResponse(responseMessage, status_code=201)
+
+@app.route(route="getAttachmentClass", auth_level=func.AuthLevel.ANONYMOUS)
+def getAttachmentClass(req: func.HttpRequest) -> func.HttpResponse:
+    fName = f"f(getAttachmentClass)->"
+    try:
+        messageId, reqBody = getHttpRequestBody(request = req, functionName=fName)
+        logging.info(f'{fName}Received MessageId:{messageId}')
+        logging.info(f'{fName}Received request body:{reqBody}')
+    except Exception as httpRequestErrorMessage:
+        return func.HttpResponse(f'{httpRequestErrorMessage}', status_code=400)
+    try:
+        messageType = getItemFromRequestBody(reqBody, 'messageType', fName)
+        receivedTimeFolder = getItemFromRequestBody(reqBody, 'receivedTimeFolder', fName)
+        sender = getItemFromRequestBody(reqBody, 'from', fName)
+        messageUri = getItemFromRequestBody(reqBody, 'uri', fName)
+        attachmentName = getItemFromRequestBody(reqBody, 'attachmentName', fName)       
+        url = messageUri + sender + "/" + receivedTimeFolder + "/attachments/" + attachmentName
+        if messageType == 'email-attachment':
+            try:
+                attachmentClasses = getAttachmentClassesFromFormRecognizer(
+                                    url, 
+                                    fName
+                                )
+            except Exception as e:
+                return func.HttpResponse(f'{e}', status_code=400)               
+        else:
+            errorMessage = f'{fName}ERROR: incorrect messageType {messageType}'
+            logging.error(errorMessage)
+            return func.HttpResponse(errorMessage, status_code=400)
+    except Exception as httpRequestErrorMessage:
+        errorMessage = f'{fName}ERROR: Read request body items raised exception:{httpRequestErrorMessage}'
+        logging.error(errorMessage)
+        return func.HttpResponse(errorMessage, status_code=400)
+
+    logging.info(f'{fName}Success: attachment {attachmentName} has the classes: {attachmentClasses}') 
+    return func.HttpResponse(f'{attachmentClasses}', status_code=200)    
+
+@app.route(route="saveAttachmentProperties", auth_level=func.AuthLevel.ANONYMOUS)
+@app.queue_output(arg_name="msg", queue_name="outqueue", connection="AzureWebJobsStorage")
+@app.cosmos_db_output(arg_name="outputDocument", database_name="DocAIDatabase", 
+    container_name="EmailExtracts", connection="CosmosDbConnectionString")
+def saveAttachmentProperties(req: func.HttpRequest,
+                        msg: func.Out[func.QueueMessage], 
+                        outputDocument: func.Out[func.Document]) -> func.HttpResponse:
+    fName = f"f(saveAttachmentProperties)->"
+    try:
+        messageId, reqBody = getHttpRequestBody(request = req, functionName=fName)
+        logging.info(f'{fName}Received MessageId:{messageId}')
+        logging.info(f'{fName}Received request body:{reqBody}')
+    except Exception as httpRequestErrorMessage:
+        return func.HttpResponse(f'{httpRequestErrorMessage}', status_code=400)
+    try:
+        messageType = getItemFromRequestBody(reqBody, 'messageType', fName)
+        if messageType == 'email-attachment':
+                receivedTimeFolder = getItemFromRequestBody(reqBody, 'receivedTimeFolder', fName)
+                receivedTime = getItemFromRequestBody(reqBody, 'receivedTime', fName)
+                sender = getItemFromRequestBody(reqBody, 'from', fName)
+                attachmentClasses = getItemFromRequestBody(reqBody, 'classes', fName)
+                attachmentName = getItemFromRequestBody(reqBody, 'attachmentName', fName)               
+                messageUri = getItemFromRequestBody(reqBody, 'uri', fName)
+                url = messageUri + sender + "/" + receivedTimeFolder + "/attachments/" + attachmentName
+        else:
+            errorMessage = f'{fName}ERROR: incorrect messageType {messageType}'
+            logging.error(errorMessage)
+            return func.HttpResponse(errorMessage, status_code=400)
+    except Exception as httpRequestErrorMessage:
+        errorMessage = f'{fName}ERROR: Read request body items raised exception:{httpRequestErrorMessage}'
+        logging.error(errorMessage)
+        return func.HttpResponse(errorMessage, status_code=400)   
+    doc = {
+        "id":str(uuid.uuid4()),
+        "messageId":messageId,
+        "messageType":messageType,
+        "receivedTime":receivedTime,
+        "receivedTimeFolder":receivedTimeFolder,
+        "from":sender,
+        "classes":attachmentClasses,
+        "attachmentName":attachmentName,
+        "url":url,
+        "messageDetails":reqBody
+    }
+    logging.info(f'{fName}Creating response payload json')
+    try:
+        inJSON = json.dumps(doc)
+        logging.info(f'{fName}Document to store in CosmosDB: {inJSON}')
+    except Exception as je:
+        errorMessage = f'{fName}Error: json dump pf response payload failed. Exception:{je}'
+        logging.error(errorMessage)
+        return func.HttpResponse(errorMessage, status_code=400)
+    
+    outputDocument.set(func.Document.from_json(inJSON))
+    msg.set(messageId)
+    responseMessage = f'{fName}messageId  {messageId} data stored in Cosmos DB'
     logging.info(responseMessage)
     return func.HttpResponse(responseMessage, status_code=201)

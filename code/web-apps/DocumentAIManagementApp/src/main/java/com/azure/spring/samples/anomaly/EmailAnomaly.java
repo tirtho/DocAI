137da514dfd60756dc009b939975f5b7553440b7
Tirthankar Barari <tirtho@hotmail.com>
2023-12-30 09:06:16 -0500

    added the first anomaly class

---
+++ code/web-apps/DocumentAIManagementApp/src/main/java/com/azure/spring/samples/anomaly/EmailAnomaly.java
@@ -0,0 +1,25 @@
+package com.azure.spring.samples.anomaly;
+
+public class EmailAnomaly {
+
+	/**
+	 * Find intent from email
+	 * Check if necessary information has been provided
+	 * 
+	 * @return String response
+	 */
+	public String checkIntentContentGap() {
+		
+		return null;
+	}
+	
+	/**
+	 * Any PII data or content moderation needed? 
+	 * 
+	 * @return
+	 */
+	public String checkContentModeration() {
+		
+		return null;
+	}
+}

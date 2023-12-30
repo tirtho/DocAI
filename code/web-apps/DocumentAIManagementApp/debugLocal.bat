e31aba2847e72cb8d5da31c00b651c3325f2ac1b
Tirthankar Barari <tirtho@hotmail.com>
2023-12-30 13:58:01 -0500

    build and deploy cleanups. now works for local debug, run and deployed app in azure web app!

---
+++ code/web-apps/DocumentAIManagementApp/debugLocal.bat
@@ -0,0 +1,15 @@
+set COSMOS_KEY=%COSMOSDB_API_KEY%
+set COSMOS_URI=%COSMOSDB_URI%
+set COSMOS_DATABASE=DocAIDatabase
+set COSMOS_CONTAINER=EmailExtracts
+set COGNITIVE_SERVICE_KEY=%ALL_AI_SERVICES_API_KEY%
+set COGNITIVE_SERVICE_ENDPOINT=%ALL_AI_SERVICES_ENDPOINT%
+set BING_KEY=%BING_API_KEY%
+set BING_QUERY_COUNT=3
+
+copy pom.xml.debug pom.xml
+
+echo "Make sure you have Eclipse is running and setup Eclipse Debug Configuration with Standard socket, localhost, 9090"
+echo "After maven starts app locally, run Debug in Eclipse"
+
+mvn package spring-boot:run

dc23a568f557e1000250af62016c50f2946de456
Tirthankar Barari <tirtho@hotmail.com>
2025-01-03 14:57:06 -0500

    update

--- code/python/functions/aoai.py
+++ code/python/functions/aoai.py
@@ -48,6 +48,58 @@ def getChatCompletion(the_engine, the_messages):
     )
     return response.usage.total_tokens, response.choices[0].finish_reason, response.choices[0].message["content"]
 
+# For models called using Http requests
+def getHttpChatCompletion(
+      fName, 
+      aoaiEndpointEnv, 
+      aoaiKeyEnv, 
+      aoaiAPIVersionEnv, 
+      aoaiAPIEngineEnv, 
+      prompt
+    ):
+    fName = f"{fName}f(getHttpChatCompletion)->"
+    try:
+      if aoaiKeyEnv:
+        aoaiAPIKey = os.getenv(aoaiKeyEnv)
+      if aoaiAPIKey and not aoaiAPIKey.isspace():
+        # the string is non-empty
+        print(f'\n{fName}Got API Key from environment variable {aoaiKeyEnv}')
+      else:
+        # Get key using Managed Identity
+        print(f'\n{fName}Could not get API key from environment variable {aoaiKeyEnv}. Trying Managed ID')
+        default_credential = DefaultAzureCredential()
+        token = default_credential.get_token("https://cognitiveservices.azure.com/.default")
+        aoaiAPIKey = token.token
+        print(f"\n{fName}Authenticated successfully with AAD token")        
+    except:
+        print(f'\n{fName}Failed to get AOAI API key')
+        return -1
+
+    aoaiAPIEndpoint = os.getenv(aoaiEndpointEnv)
+    aoaiAPIVersion = os.getenv(aoaiAPIVersionEnv)
+    aoaiAPIEngine = os.getenv(aoaiAPIEngineEnv)
+    rawToken = str(os.getenv('BLOB_STORE_SAS_TOKEN'))
+    decodedBytes = base64.b64decode(rawToken)
+    blobStoreSASToken = decodedBytes.decode("utf-8")[2:-2]
+    logging.info(f'{fName}Blob SAS token:{blobStoreSASToken}')        
+    endpoint = f'{aoaiMultiModalAPIEndpoint}openai/deployments/{aoaiOmniAPIEngine}/chat/completions?api-version={aoaiOmniAPIVersion}'
+    gotPrompt = composeMultiModalPrompt(f'{url}?{blobStoreSASToken}', fName)
+    logging.info(f'{fName}Got Prompt: {gotPrompt}')
+    headers = {
+                "Content-Type": "application/json",   
+                "api-key": aoaiMultiModalAPIKey 
+            }
+    response = runHttpRequest(endpoint=endpoint,
+                   headers=headers,
+                   requestType="POST",
+                   jsonRequestBody=gotPrompt,
+                   fName=fName
+                )
+    #response = requests.post(endpoint, headers=headers, data=json.dumps(gotPrompt))
+    jsonResponse = json.loads(response.content.decode('utf-8'))
+    logging.info(f'{fName} GPT4o API response:{jsonResponse}')
+  
+
 # Return vectors for a given text using the ADA model
 def generate_embedding(the_engine, the_text):
     response = openai.Embedding.create(
